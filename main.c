df141a66577d6d87a4907a8822172e68e12650f2
William Chang <williamchang@WilliamdeMacBook-Pro.local>
2023-05-03 01:44:12 +0800

    new

--- main.c
+++ main.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include "color.h"
 
-//1: Hills - bricks
-//2: Fields - grain
-//3: Pasture - sheep
-//4: Forest - lumber
-//5: Mountains - ore
-//6: Desert - no resource
-//7: Robber - no resource
-enum TileType{ Tile-Hills = 1, Tile-Fields , Tile-Pasture, Tile-Forest, Tile-Mountains, Tile-Desert };
+//1: Hills - bricks - 3
+//2: Fields - grain - 4
+//3: Pasture - sheep - 4 
+//4: Forest - lumber - 4
+//5: Mountains - ore - 3
+//6: Desert - no resource - 1
+enum TileType{ Tile_Hills , Tile_Fields  , Tile_Pasture, Tile_Forest, Tile_Mountains, Tile_Desert };
 
 int32_t tile_type[19];//1-6
-//{ 5 , 2 , 2 , 4 , 1 , 2 , 3 , 3 , .... };
+//{ 5 , 2 , 2 , 4 , 1 , 2 , 3 , 3 , 5 , 1 , 4 , 2 , 1 , 4 , 5 , 2 , 4 , 3 , 6 };
+//starting from upper-left corner and go counter-clockwise
 
 int32_t tile_dice_num[18] = { 5 , 2 , 6 , 3 , 8 , 10 , 9 , 12 , 11 , 4 , 8 , 10 , 9 , 4 , 5 , 6 , 3 , 11 };//2-12
 //{A-5} {B-2} {C-6} {D-3} {E-8} {F-10} {G-9} {H-12} {I-11} {J-4} {K-8} {L-10} {M-9} {N-4} {O-5} {P-6} {Q-3} {R-11} 
@@ -70,11 +72,11 @@ void init_node( int32_t i )
 
 int main ()
 {
-    printf("   *---*  \n");
+    printf(CLR_BOLD CLR_GRN"   *---*  \n"CLR_RST);
     printf("  /     \\ \n");
-    printf(" *       *\n");
+    printf(CLR_FLASH CLR_CYN" *       *\n"CLR_RST);
     printf("  \\     /\n");
-    printf("   *---*\n");
+    printf(CLR_24_FG(244,164,96)"   *---*\n"CLR_RST);
 
     
     return 0;
