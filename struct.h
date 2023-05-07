859db8f12d5caebf41e9a42a969f592016fe15c4
William Chang <williamchang@WilliamdeMacBook-Pro.local>
2023-05-07 23:50:34 +0800

    Update struct.h

--- struct.h
+++ struct.h
@@ -20,32 +20,6 @@ int32_t tile_type[19];//1-6
 int32_t tile_dice_num[18] = { 5 , 2 , 6 , 3 , 8 , 10 , 9 , 12 , 11 , 4 , 8 , 10 , 9 , 4 , 5 , 6 , 3 , 11 };//2-12
 //{A-5} {B-2} {C-6} {D-3} {E-8} {F-10} {G-9} {H-12} {I-11} {J-4} {K-8} {L-10} {M-9} {N-4} {O-5} {P-6} {Q-3} {R-11} 
 
-enum Resource{ Brick = 0 , Grain , Sheep , Lumber , Ore };
-
-typedef struct _sPlayer
-{
-    int32_t index;//0-3
-    int32_t color;//0-3
-    int32_t resource[5];//0-4
-    int32_t victory_point;//0-10
-    int32_t road_num;//0-15
-    int32_t settlement_num;//0-5  
-    //int32_t settlement_index[54];
-    int32_t city_num;//0-4
-    //int32_t city_index[54];
-    int32_t knight_num;//0-5
-    int32_t longest_road;//0,1
-    int32_t largest_army;//0,1
-}Player;
-
-typedef struct _sTile
-{
-    int32_t index;//1-19
-    int32_t type;//1-6
-    int32_t robber;//0,1
-    int32_t dice_num;//2-12
-}tile;
-
 typedef struct _sNode
 {
     int32_t index;//1-54
@@ -66,6 +40,36 @@ typedef struct _sNode
     int32_t tile2;
     int32_t tile3;
 }node;
+
+
+typedef struct _sTile
+{
+    int32_t index;//1-19
+    int32_t type;//1-6
+    int32_t robber;//0,1
+    int32_t dice_num;//2-12
+    int32_t resoource_type;//0-4
+    struct _sNode* nodes[6]; 
+}tile;
+
+
+enum Resource{ Brick = 0 , Grain , Sheep , Lumber , Ore };
+
+typedef struct _sPlayer
+{
+    int32_t index;//0-3
+    int32_t color;//0-3
+    int32_t resource[5];//0-4
+    int32_t victory_point;//0-10
+    int32_t road_num;//0-15
+    int32_t settlement_num;//0-5  
+    int32_t building_index[54];//0-53 0,1,2
+    int32_t city_num;//0-4
+    int32_t knight_num;//0-5
+    int32_t longest_road;//0,1
+    int32_t largest_army;//0,1
+}Player;
+
 
 int32_t road_built[72];//0,1,2,3,4
 
