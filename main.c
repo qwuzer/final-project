b39babbfdee41a4653a14fcdd6bd2ea49896959c
William Chang <williamchang@WilliamdeMacBook-Pro.local>
2023-05-10 15:05:27 +0800

    update procedure for check functions

--- main.c
+++ main.c
@@ -27,15 +27,15 @@ void obtain_resources( int32_t sum , Player *player[] , tile *ptile )
         {
             for( int32_t j = 0 ; j < 6 ; j++ )
             {
-                if( ptile[i].node[j].owner != 0 )//if there is a building on the node
+                if( ptile[i].node[j]->owner != 0 )//if there is a building on the node
                 {
-                    if( ptile[i].node[j].type = 1 )//settlment 
+                    if( ptile[i].node[j]->type = 1 )//settlment 
                     {
-                        player[ptile[i].node[j].owner].resource[ptile[i].resource_type] += ptile[i].recsource[j];
+                        player[ptile[i].node[j]->owner]->resource[ptile[i].resource_type] += ptile[i].recsource[j];
                     }
                     else if( ptile[i].node[j].type = 2 )//city
                     {
-                        player[ptile[i].node[j].owner].resource[ptile[i].resource_type] += (ptile[i].recsource[j] * 2);
+                        player[ptile[i].node[j]->owner]->resource[ptile[i].resource_type] += (ptile[i].recsource[j] * 2);
                     }
                     
                 }
@@ -123,6 +123,8 @@ int32_t print_option_menu( int32_t option[] , size_t size )
 }
 
 
+
+enum marine_trade_opt { no_port = 0 , normal_port = 1 , brick_port = 2 , wood_port = 3 , sheep_port = 4 , grain_port = 5 , ore_port = 6};
 /*IF ANY BUILD OF USE CARD FUNC IS SUCCESSFUL , BE SURE TO DECREASE CORRESPONDING VALUES*/
 int32_t if_settlement_on_port( Player player )
 {
@@ -144,7 +146,7 @@ int32_t if_settlement_on_port( Player player )
 }
 
 
-int32_t marine_trade( Player *player )
+int32_t marine_trade_chk( Player *player )
 {
     int32_t on_port = if_settlement_on_port( *player );
     if( on_port == 1 )//normal port
@@ -152,13 +154,15 @@ int32_t marine_trade( Player *player )
         if( player->wood >= 3 || player->brick >= 3 || player->sheep >= 3 || player->wheat >= 3 || player->ore >= 3 )
         {
             //offer trade
+            return 1;
         }
     }
     else if( on_port != 0 )//any special port
     {
         switch( on_port )
         {
-            //...
+            //check port cond
+            //return port num
         }
     }
     else if ( player->wood >= 4 || player->brick >= 4 || player->sheep >= 4 || player->wheat >= 4 || player->ore >= 4 )
@@ -180,27 +184,61 @@ int32_t player_trade( struct player player )
 }
 */
 
-int32_t able_to_build( struct player player )
+
+//other restrictions? e.g development card can only be used once per turn
+int32_t is_able_to( Player *player )
 {
-    if( player.wood >= 1 && player.brick >= 1 && player.sheep >= 1 && player.wheat >= 1 )
+    if( player->sheep >= 1 && player->wheat >= 1 && player->ore >= 1 )
+    {
+        option[buy_dev_card] = 1;
+        return 1;
+    }
+
+    if( player->dev_card_num >= 1 )
+    {
+        option[use_dev_card] = 1;
+        return 1;
+    }
+
+    if( player->wood >= 1 || player->brick >= 1 || player->sheep >= 1 || player->wheat >= 1 )
+    {
+        option[trade_with_player] = 1;
+        return 1;
+    }
+
+    if( int32_t port_type = marine_trade_chk( player ) )
+    {
+        option[marine_trade] = port_type;
+        return 1;
+    }
+
+    if( player->wood >= 1 && player->brick >= 1 && player->sheep >= 1 && player->wheat >= 1 )
     {
         option[build_settlement] = 1;
         return 1;
     }
 
-    if( player.wood >= 1 && player.brick >= 1 )
+    if( player->wood >= 1 && player->brick >= 1 )
     {
         option[build_road] = 1;
         return 1;
     }
 
-    if( player.wheat >= 2 && player.ore >= 3 && player.settlement_num >= 1 )
+    if( player->wheat >= 2 && player->ore >= 3 && player->settlement_num >= 1 )
     {
         option[build_city] = 1;
         return 1;
     }
 
     return 0;
+}
+
+void print_build( Player player )
+{
+    int32_t select = 0;
+    printf("Select a place to build your settlement: \n");
+    scanf("%d",&select);
+    //check if the node is legal to build       
 }
 
 
@@ -231,27 +269,6 @@ build_road()
     
 }
 
-
-int32_t able_buy_dev_card( Player player )
-{
-    if( player.sheep >= 1 && player.wheat >= 1 && player.ore >= 1 )
-    {
-        option[buy_dev_card] = 1;
-        return 1;
-    }
-    return 0;
-}
-
-int32_t able_use_dev_card( Player player )
-{
-    if( player.dev_card_num >= 1 )
-    {
-        option[use_dev_card] = 1;
-        return 1;
-    }
-    return 0;
-}
-
 //check if dev is obtained in the same turn, if yes, unable to use
 buy_dev_card()
 {
@@ -280,16 +297,50 @@ int main ()
             case 1:
                 printf( "Player 1's turn\n" );
                 int32_t choice;
-                while( (choice = print_option_menu( option , 6 )) != -1 )
+                while( is_able_to( player1 ) )
                 {
-                    switch(choice)
+                    int32_t c = print_option_menu( option , 6 );
+                    int32_t choice = 0;
+                    //this is a terrible design but bear with me :(
+                    for( int32_t i = 0 ; i < 7 ; i++ )
+                    {   
+                        if( option[i] == 1 )
+                        {
+                            choice++;
+                        }
+                        if( choice == c )
+                        {
+                            choice = i;
+                            break;
+                        }
+                    }
+                    switch( choice )
                     {
-                        case 1:
-                            if( able_buy_dev_card(player1) )
+                        case 1: 
+                            buy_dev_card( player1 );
+                            break;
+                        case 2:
+                            use_dev_card( player1 );
+                            break;
+                        case 3:
+                            trade_with_player( player1 );
+                            break;
+                        case 4:
+                            marine_trade( player1 );
+                            break;
+                        case 5:
+                            build_settlement( player1 );
+                            break;
+                        case 6:
+                            build_road( player1 );
+                            break;
+                        case 7:
+                            build_city( player1 );
+                            break;
+                        default:
+                            break;
                     }
                 }
-                is_game_over();
-                print_option_menu( option , 6 );
                 break;
             case 2:
                 printf( "Player 2's turn\n" );
