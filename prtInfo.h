248c876b8a1d5892357ee2baf766b9e294cfdb40
Marja <114715095+marjasback2bedge@users.noreply.github.com>
2023-06-17 19:12:53 +0800

    Add files via upload

---
+++ prtInfo.h
@@ -0,0 +1,6 @@
+#pragma once
+#include "struct.h"
+
+void prt( int32_t index, char * str , int32_t o); 
+void prtNode( Player * player, road * proad, node * pnode );
+void prtRoad( Player * player, road * proad );
