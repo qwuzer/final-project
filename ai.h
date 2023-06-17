a36680096bbac5f391b36f42774db23ee04bb814
morris46202 <morris46202@gmail.com>
2023-06-17 17:49:47 +0800

    修改ai.h

--- ai.h
+++ ai.h
@@ -5,28 +5,38 @@
 
 // ai = "a" lot of "i"f-else 
 
+typedef struct _arg
+{
+    Player *p;
+    tile *t;
+    node *n;
+    road *r;
+    int32_t me;
+}Arg;
+
 int32_t roll_the_dice();
 
 // for basic
 //public function
-void ai_turn(); 
+void ai_turn( Player *p , tile *t , node *n , road *r ); 
 //private function
-void ai_set_road();
-void ai_set_village();
-void ai_upgrade();
-void ai_find_empty_road();
-void ai_find_empty_node();
-void ai_find_village();
-void ai_move_robber();
+void ai_set_road( int32_t where , Arg arg );
+void ai_set_village( int32_t where , Arg arg );
+void ai_upgrade( int32_t where , Arg arg );
+int32_t ai_find_empty_road( Arg arg );
+int32_t ai_find_empty_node( Arg arg );
+int32_t ai_find_village( Arg arg );
+void ai_move_robber( int32_t where , Arg arg );
+void ai_get_resourse( int32_t dice , Arg arg );
 
 // for advance
 //public function
 void ai_advanced_pro_max_plus_ultra_turn();
 //private function
-void ai_find_high_cp_empty_node();
-void ai_find_high_ememy_tile();
-void ai_find_next_long_road();
-void ai_use_crad();
+int32_t ai_find_high_cp_empty_node();
+int32_t ai_find_high_ememy_tile();
+int32_t ai_find_next_long_road();
+void ai_use_card();
 void ai_judge_for_trade();
 void ai_judge_for_buy_card();
 void ai_sort_resource();
