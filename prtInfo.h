cbd72d8b15cbe18c21f77279a3d43774dc2b51ee
Marja <114715095+marjasback2bedge@users.noreply.github.com>
2023-06-18 18:58:55 +0800

    Add files via upload

--- prtInfo.h
+++ prtInfo.h
@@ -4,4 +4,4 @@
 void prt( int32_t index, char * str , int32_t owner); 
 void prtNode( Player * player, road * proad, node * pnode, int32_t build_index[54] );
 void prtRoad( Player * player, road * proad, int32_t road_index[72] );
-
+void prtTile();
